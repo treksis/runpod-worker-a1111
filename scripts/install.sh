cc292c96d4f7b03caaee7d3e214a280bb24c55a1
Ho Woo Jang <54779707+treksis@users.noreply.github.com>
2024-01-17 13:29:43 -0500

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -30,13 +30,10 @@ python3 -m install-automatic --skip-torch-cuda-test
 
 echo "Cloning ControlNet extension repo"
 cd /workspace/stable-diffusion-webui
-git clone --depth=1 https://github.com/Mikubill/sd-webui-controlnet.git extensions/sd-webui-controlnet
+git clone --depth=1 https://github.com/treksis/sd-webui-controlnet.git extensions/sd-webui-controlnet
 
 echo "Cloning the ReActor extension repo"
-git clone --depth=1 https://github.com/Gourieff/sd-webui-reactor.git extensions/sd-webui-reactor
-
-echo "Cloning the After Detailer extension repo"
-git clone --depth=1 https://github.com/Bing-su/adetailer.git extensions/adetailer
+git clone --depth=1 https://github.com/treksis/sd-webui-reactor extensions/sd-webui-reactor
 
 echo "Installing dependencies for ControlNet"
 cd /workspace/stable-diffusion-webui/extensions/sd-webui-controlnet
@@ -47,14 +44,10 @@ cd /workspace/stable-diffusion-webui/extensions/sd-webui-reactor
 pip3 install -r requirements.txt
 pip3 install onnxruntime-gpu
 
-echo "Installing dependencies for After Detailer"
-cd /workspace/stable-diffusion-webui/extensions/adetailer
-python3 -m install
-
 echo "Installing the model for ReActor"
 mkdir -p /workspace/stable-diffusion-webui/models/insightface
 cd /workspace/stable-diffusion-webui/models/insightface
-wget https://github.com/facefusion/facefusion-assets/releases/download/models/inswapper_128.onnx
+wget https://github.com/treksis/sd-webui-reactor/releases/download/Download/inswapper_128.onnx
 
 echo "Configuring ReActor to use the GPU instead of CPU"
 echo "CUDA" > /workspace/stable-diffusion-webui/extensions/sd-webui-reactor/last_device.txt
@@ -65,40 +58,16 @@ pip3 install huggingface_hub runpod
 
 echo "Downloading Deliberate v2 model"
 cd /workspace/stable-diffusion-webui/models/Stable-diffusion
-wget -O deliberate_v2.safetensors https://huggingface.co/XpucT/Deliberate/resolve/main/Deliberate_v2.safetensors
-
-echo "Downloading SDXL base model"
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
-
-echo "Downloading SDXL Refiner"
-wget https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
+wget -O absolutereality_lcm.safetensors https://huggingface.co/treksis/buffalo_l/resolve/main/absolutereality_lcm.safetensors
 
 echo "Downloading SD 1.5 VAE"
 cd /workspace/stable-diffusion-webui/models/VAE
 wget https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors
 
-echo "Downloading SDXL VAE"
-wget https://huggingface.co/madebyollin/sdxl-vae-fp16-fix/resolve/main/sdxl_vae.safetensors
-
 echo "Downloading SD 1.5 ControlNet models"
 mkdir -p /workspace/stable-diffusion-webui/models/ControlNet
 cd /workspace/stable-diffusion-webui/models/ControlNet
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_inpaint.pth
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart.pth
-wget https://huggingface.co/ioclab/ioc-controlnet/resolve/main/models/control_v1p_sd15_brightness.safetensors
-wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth
-
-echo "Downloading SDXL ControlNet models"
-wget https://huggingface.co/lllyasviel/sd_control_collection/resolve/main/diffusers_xl_canny_full.safetensors
-
-echo "Downloading Upscalers"
-mkdir -p /workspace/stable-diffusion-webui/models/ESRGAN
-cd /workspace/stable-diffusion-webui/models/ESRGAN
-wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/4x-UltraSharp.pth
-wget https://huggingface.co/ashleykleynhans/upscalers/resolve/main/lollypop.pth
+wget https://huggingface.co/treksis/buffalo_l/resolve/main/ip-adapter-faceid-plusv2_sd15.bin
 
 echo "Creating log directory"
 mkdir -p /workspace/logs
@@ -106,9 +75,9 @@ mkdir -p /workspace/logs
 echo "Installing config files"
 cd /workspace/stable-diffusion-webui
 rm webui-user.sh config.json ui-config.json
-wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/webui-user.sh
-wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/config.json
-wget https://raw.githubusercontent.com/ashleykleynhans/runpod-worker-a1111/main/ui-config.json
+wget https://raw.githubusercontent.com/treksis/runpod-worker-a1111/main/webui-user.sh
+wget https://raw.githubusercontent.com/treksis/runpod-worker-a1111/main/config.json
+wget https://raw.githubusercontent.com/treksis/runpod-worker-a1111/main/ui-config.json
 
 echo "Starting A1111 Web UI"
 deactivate
